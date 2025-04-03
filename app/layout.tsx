2c6056afd77e87bcd6ba7edee6c880d4cbe3c0e1
denuwanbogoda <binaradenuwanguitar@gmail.com>
2025-04-03 18:57:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'AIdexio',
+  description: 'Ask Anything...',
 };
 
 export const viewport = {
